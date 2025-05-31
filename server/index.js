9370420224c0ba2fde56d715587de73adf08be79
Lynn-Wanjiru <150967322+Lynn-Wanjiru@users.noreply.github.com>
2025-05-31 18:52:03 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      
       "https://trafficalertsystem.onrender.com",
     ],
     credentials: true,
